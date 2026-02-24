78fe4a74c89ab8cf5cdaa7815c717760f26443f3
INTWARI Bonheur <intwaribonheur@gmail.com>
2026-02-24 16:47:10 +0200

    gg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,15 @@ import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import SymptomChecker from './pages/SymptomChecker'
+
 import DiseaseList from './pages/DiseaseList'
 import HealthBehaviors from './pages/HealthBehaviors';
 import Profile from './pages/Profile'
 import DiseaseDetail from './pages/DiseaseDetail'
 import ProfileSetup from './pages/ProfileSetup';
 
-import PregnancyManager from './pages/PregnancyManager';
-import MyDiseases from './pages/MyDiseases';
+
+
 import Forum from './pages/Forum';
 import Group from './pages/Group';
 import CreatePost from './pages/CreatePost';
@@ -33,16 +33,16 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/symptom-checker" element={<SymptomChecker />} />
+            
             <Route path="/diseases" element={<DiseaseList />} />
            
             <Route path="/profile" element={<Profile />} />
             <Route path="/diseases/:id" element={<DiseaseDetail />} />
             <Route path="/profile-setup" element={<ProfileSetup />} />
             
-            <Route path="/pregnancy-manager" element={<PregnancyManager />} />
+            
             <Route path="/health-behaviors" element={<HealthBehaviors />} />
-            <Route path="/my-diseases" element={<MyDiseases />} />
+            
             <Route path="/forum" element={<Forum />} />
             <Route path="/group/:id" element={<Group />} />
             <Route path="/group/:id/create-post" element={<CreatePost />} />
